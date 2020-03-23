26353c7d53ae98c2f84d05cca99b7c7f82e37d29
wudiliujie@163.com <Lj61844330>
2020-03-23 14:39:23 +0800

    优化kfk消费。

--- clusterConsumer/clusterConsumer.go
+++ clusterConsumer/clusterConsumer.go
@@ -1,21 +1,23 @@
 package clusterConsumer
 
 import (
+	"context"
 	"github.com/Shopify/sarama"
-	cluster "github.com/bsm/sarama-cluster"
 	"github.com/wudiliujie/common/log"
 	"github.com/wudiliujie/common/module"
 	"strings"
+	"sync"
 )
 
 const ConsumerMsgEvent = "ConsumerMsgEvent"
 
 type ClusterConsumer struct {
-	brokers  []string
-	topics   []string
-	groupId  string
-	consumer *cluster.Consumer
-	closeSig chan bool
+	brokers           []string
+	topics            []string
+	groupId           string
+	consumer          sarama.ConsumerGroup
+	closeSig          chan bool
+	partitionConsumer []sarama.PartitionConsumer
 }
 
 func NewClusterConsumer(brokers string, topics string, groupId string) *ClusterConsumer {
@@ -28,42 +30,76 @@ func NewClusterConsumer(brokers string, topics string, groupId string) *ClusterC
 }
 func (c *ClusterConsumer) Init() error {
 	var err error
-	config := cluster.NewConfig()
-	config.Consumer.Return.Errors = true
-	config.Group.Return.Notifications = true
+	config := sarama.NewConfig()
 	config.Consumer.Offsets.Initial = sarama.OffsetOldest
-	c.consumer, err = cluster.NewConsumer(c.brokers, c.groupId, c.topics, config)
+	config.Version = sarama.V1_0_0_0
+	c.consumer, err = sarama.NewConsumerGroup(c.brokers, c.groupId+"aaa", config)
 	return err
 }
 func (c *ClusterConsumer) Run() {
-	for {
-		select {
-		case <-c.closeSig:
-			log.Debug("close")
-			return
-		case msg, ok := <-c.consumer.Messages():
-			if ok {
-				log.Debug("msg:%v %v", msg.Offset, msg.Topic)
-				module.OnChanRpcEvent(ConsumerMsgEvent, c, msg)
-				//consumer.MarkOffset(msg, "")
+	consumer := Consumer{
+		ready: make(chan bool),
+	}
+	ctx, cancel := context.WithCancel(context.Background())
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for {
+			// `Consume` should be called inside an infinite loop, when a
+			// server-side rebalance happens, the consumer session will need to be
+			// recreated to get the new claims
+			if err := c.consumer.Consume(ctx, c.topics, &consumer); err != nil {
+				log.Fatal("Error from consumer: %v", err)
 			}
-			break
-		case ntf := <-c.consumer.Notifications():
-			if ntf != nil {
-				log.Debug("notifications %v", ntf)
+			// check if context was cancelled, signaling that the consumer should stop
+			if ctx.Err() != nil {
+				return
 			}
-			break
-		case err := <-c.consumer.Errors():
-			if err != nil {
-				log.Error("kfk进程异常 %v", err)
-			}
-			break
+			consumer.ready = make(chan bool)
 		}
+	}()
+	<-consumer.ready // Await till the consumer has been set up
+	select {
+	case <-ctx.Done():
+		log.Debug("terminating: context cancelled")
+	case <-c.closeSig:
+		log.Debug("terminating: via signal")
+	}
+	cancel()
+	wg.Wait()
+	if err := c.consumer.Close(); err != nil {
+		log.Fatal("Error closing client: %v", err)
 	}
 }
-func (c *ClusterConsumer) MarkOffset(msg *sarama.ConsumerMessage) {
-	c.consumer.MarkOffset(msg, "")
-}
+
 func (c *ClusterConsumer) Close() {
 	c.closeSig <- true
+}
+
+// Consumer represents a Sarama consumer group consumer
+type Consumer struct {
+	ready chan bool
+}
+
+// Setup is run at the beginning of a new session, before ConsumeClaim
+func (consumer *Consumer) Setup(sarama.ConsumerGroupSession) error {
+	// Mark the consumer as ready
+	close(consumer.ready)
+	return nil
+}
+
+// Cleanup is run at the end of a session, once all ConsumeClaim goroutines have exited
+func (consumer *Consumer) Cleanup(sarama.ConsumerGroupSession) error {
+	return nil
+}
+
+// ConsumeClaim must start a consumer loop of ConsumerGroupClaim's Messages().
+func (consumer *Consumer) ConsumeClaim(session sarama.ConsumerGroupSession, claim sarama.ConsumerGroupClaim) error {
+	for message := range claim.Messages() {
+		module.OnChanRpcEvent(ConsumerMsgEvent, message)
+		session.MarkMessage(message, "")
+	}
+
+	return nil
 }
--- go.mod
+++ go.mod
@@ -3,17 +3,13 @@ module github.com/wudiliujie/common
 go 1.12
 
 require (
-	github.com/Shopify/sarama v1.23.1
+	github.com/Shopify/sarama v1.26.1
 	github.com/bsm/sarama-cluster v2.1.15+incompatible
-	github.com/go-redis/redis v6.15.2+incompatible
-	github.com/go-sql-driver/mysql v1.4.1
-	github.com/golang/protobuf v1.3.1
+	github.com/go-redis/redis v6.15.7+incompatible
+	github.com/go-sql-driver/mysql v1.5.0
+	github.com/golang/protobuf v1.3.5
 	github.com/gorilla/websocket v1.4.1
-	github.com/kr/pretty v0.1.0 // indirect
-	github.com/onsi/ginkgo v1.9.0 // indirect
-	github.com/onsi/gomega v1.6.0 // indirect
+	github.com/onsi/ginkgo v1.12.0 // indirect
+	github.com/onsi/gomega v1.9.0 // indirect
 	github.com/satori/go.uuid v1.2.0
-	google.golang.org/appengine v1.6.1 // indirect
-	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
-	gopkg.in/jcmturner/goidentity.v3 v3.0.0 // indirect
 )
